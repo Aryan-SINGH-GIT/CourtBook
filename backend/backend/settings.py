adc2e68a4ee56d8d64382b4f5a0d1154e85ed800
Aryan-SINGH-GIT <aryansingh12oct2005@gmail.com>
2025-12-16 10:12:46 +0530

    Relax static file storage strictness
    Signed-off-by: Aryan-SINGH-GIT <aryansingh12oct2005@gmail.com>

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
